afbc4abbe8a0d8d5dc7aa38d3c24d4ef7caeb23c
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2024-09-04 13:56:04 +0530

    added notes of CD & SE

--- assets/js/fileUrls5thNote.js
+++ assets/js/fileUrls5thNote.js
@@ -16,6 +16,66 @@ const fileUrls5thNote = {
 
     // SE = Software Engineering
     "SE_Software_Engineering": ['https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/SE%20Slides.pdf'],
+    "SE_Software_Engg_Course_Handout": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/01_Software%20Engg%20Course%20Handout%202024%20ver0.6%20(1).pdf"],
+    "SE_Examples_on_Testing": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/Examples%20on%20Testing.ppt"],
+    "SE_Hands_on_Projects": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/HANDS-ON%20%20PROJECTS.docx"],
+    "SE_L14_Structured_Design": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/L14_StructuredDesign.ppt"],
+    "SE_Lect03_Requirements_Analysis_and_Specification": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT03_Requirements%20Analysis%20and%20Specification.PPT"],
+    "SE_Lect10_Software_Project_Management": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT10_Software%20Project%20Management.PPT"],
+    "SE_Lect11A_Software_Testing": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT11A_Software%20Testing.PPT"],
+    "SE_Lect11B_Software_Testing": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT11B_Software%20Testing.PPT"],
+    "SE_Lect11_Testing_and_Debugging": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT11_Testing%20and%20Debugging.PPT"],
+    "SE_Lect12_Software_Maintenance_and_CASE": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT12_Software%20Maintenance%20and%20Computer%20Aided%20Software%20Engineering%20%20(CASE).PPT"],
+    "SE_Lect13_Software_Reliability": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT13_Software%20Reliability.PPT"],
+    "SE_Lect14_Software_Quality_Assurance": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT14_Software%20Quality%20Assurance.PPT"],
+    "SE_Lect15_Software_Reuse": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT15_Software%20Reuse.PPT"],
+    "SE_Lect16_Software_Safety_and_Halstead_Software": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT16_Software%20Safety%20and%20Halstead%E2%80%99s%20Software.PPT"],
+    "SE_Lect1_Introduction": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT1_Introdution.PPT"],
+    "SE_Lect2_Life_Cycle_Models": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT2%20_Life%20Cycle%20Models.PPT"],
+    "SE_Lect3A_Formal_Specification_of_a_system": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT3A_Formal%20Specification%20of%20a%20system.PPT"],
+    "SE_Lect3B_Formal_Specification": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT3B_Formal%20Specification.PPT"],
+    "SE_Lect3_Formal_Specification_of_Systems_Lecture": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT3_Formal%20Specification%20of%20Systems%20Lecture.PPT"],
+    "SE_Lect3_Formal_Specification": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT3_Formal%20Specification.PPT"],
+    "SE_Lect4A_Software_Design": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT4A_Software%20Design.PPT"],
+    "SE_Lect4_Software_Design": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT4_Software%20Design.PPT"],
+    "SE_Lect5_Function_Oriented_Software_Design": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT5_Function-Oriented%20Software%20Design.PPT"],
+    "SE_Lect6A_Function_Oriented_Software_Design": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT6A_Function-Oriented%20Software%20Design.PPT"],
+    "SE_Lect6_Function_Oriented_Software_Design": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT6_Function-Oriented%20Software%20Design.PPT"],
+    "SE_Lect7_User_Interface_Design": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT7_User%20Interface%20Design.PPT"],
+    "SE_Lect8_GUI_Design_and_Coding": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT8_GUI%20Design%20and%20Coding.PPT"],
+    "SE_Lect9_Software_Project_Management": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/SE/LECT9_Software%20Project%20Management.PPT"],
+
+
+
+
+    // CD = Compiler Design
+    "CD_Complete_notes": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/CD/CD%20Complete%20notes.pdf"],
+    "CD_Full_Slides": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/CD/CD%20Full%20Slides.pdf"],
+    "CD_Notes_handwritten": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/CD/CD%20Notes%20handwritten.pdf"],
+    "CD_Compiler_Design": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/CD/Compiler%20Design.pdf"],
+
+    // DOS = Distributive Operating System
+    "DOS_Lesson_Plan": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/Lesson-Plan-DOS-2024-Autumn-SCR.docx"],
+    "DOS_Numericals_Unit_II": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/Numericals_Unit-II.txt"],
+    "DOS_U1_L1_Introduction_to_distributed_systems": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U1_L1_Introduction%20to%20distributed%20systems.pptx"],
+    "DOS_U1_L2_Hardware_Concepts": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U1_L2_Hardware%20Concepts.pptx"],
+    "DOS_U1_L3_Software_Concepts": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U1_L3_Software%20Concepts.pptx"],
+    "DOS_U1_L4_Design_Issues": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U1_L4_Design%20Issues.pptx"],
+    "DOS_U2_L07_client_server": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U2_L07-client%20server.pptx"],
+    "DOS_U2_L08_Blocking_Vs_Nonblocking_primitives_and_Buffered_Vs_Unbuffered_primitives": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U2_L08_Blocking-Vs-Nonblocking-primitives_and_Buffered-Vs-Unbuffered-primitives.pptx"],
+    "DOS_U2_L09_Reliable_Vs_Unreliable_primitives": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U2_L09_Reliable-Vs-Unreliable-primitives.pptx"],
+    "DOS_U2_L10_RPC": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U2_L10_RPC.pptx"],
+    "DOS_U2_L12_RPC_Performance": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U2_L12-RPC-Performance.pptx"],
+    "DOS_U2_L5_Layered_Protocols": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U2_L5_Layered%20Protocols.pptx"],
+    "DOS_U2_L6_Asynchronous_Transfer_Mode_ATM_Networks": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U2_L6_Asynchronous%20Transfer%20Mode%20(ATM)%20Networks.pptx"],
+    "DOS_U3_L13_Clock_Synchronization": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/U3_L13_Clock_Synchronization.pptx"],
+    "DOS_UNIT_1_Practise_Questions": ["https://github.com/bibek10550/files/blob/main/trash/bin/download/KIIT%202022/B.Tech%205th%20semester/DOS/UNIT_1_Practise%20Questions.docx"],
+
+
+
+
+
+
 
 
 
@@ -112,15 +172,65 @@ updateSubjectNoteData(
 
     // Software Engineering
     "Software Engineering", "Slides", "SE_Software_Engineering",
+    "Software Engineering", "Software Engg Course Handout", "SE_Software_Engg_Course_Handout",
+    "Software Engineering", "Lect1 Introduction", "SE_Lect1_Introduction",
+    "Software Engineering", "Lect2 Life Cycle Models", "SE_Lect2_Life_Cycle_Models",
+    "Software Engineering", "Lect03 Requirements Analysis and Specification", "SE_Lect03_Requirements_Analysis_and_Specification",
+    "Software Engineering", "Lect3 Formal Specification of Systems Lecture", "SE_Lect3_Formal_Specification_of_Systems_Lecture",
+    "Software Engineering", "Lect3 Formal Specification", "SE_Lect3_Formal_Specification",
+    "Software Engineering", "Lect3A Formal Specification of a system", "SE_Lect3A_Formal_Specification_of_a_system",
+    "Software Engineering", "Lect3B Formal Specification", "SE_Lect3B_Formal_Specification",
+    "Software Engineering", "Lect4 Software Design", "SE_Lect4_Software_Design",
+    "Software Engineering", "Lect4A Software Design", "SE_Lect4A_Software_Design",
+    "Software Engineering", "Lect5 Function-Oriented Software Design", "SE_Lect5_Function_Oriented_Software_Design",
+    "Software Engineering", "Lect6 Function-Oriented Software Design", "SE_Lect6_Function_Oriented_Software_Design",
+    "Software Engineering", "Lect6A Function-Oriented Software Design", "SE_Lect6A_Function_Oriented_Software_Design",
+    "Software Engineering", "Lect7 User Interface Design", "SE_Lect7_User_Interface_Design",
+    "Software Engineering", "Lect8 GUI Design and Coding", "SE_Lect8_GUI_Design_and_Coding",
+    "Software Engineering", "Lect9 Software Project Management", "SE_Lect9_Software_Project_Management",
+    "Software Engineering", "Lect10 Software Project Management", "SE_Lect10_Software_Project_Management",
+    "Software Engineering", "Lect11 Testing and Debugging", "SE_Lect11_Testing_and_Debugging",
+    "Software Engineering", "Lect11A Software Testing", "SE_Lect11A_Software_Testing",
+    "Software Engineering", "Lect11B Software Testing", "SE_Lect11B_Software_Testing",
+    "Software Engineering", "Lect12 Software Maintenance and CASE", "SE_Lect12_Software_Maintenance_and_CASE",
+    "Software Engineering", "Lect13 Software Reliability", "SE_Lect13_Software_Reliability",
+    "Software Engineering", "Lect14 Software Quality Assurance", "SE_Lect14_Software_Quality_Assurance",
+    "Software Engineering", "L14 Structured Design", "SE_L14_Structured_Design",
+    "Software Engineering", "Lect15 Software Reuse", "SE_Lect15_Software_Reuse",
+    "Software Engineering", "Lect16 Software Safety and Halstead Software", "SE_Lect16_Software_Safety_and_Halstead_Software",
+    "Software Engineering", "Examples on Testing", "SE_Examples_on_Testing",
+    "Software Engineering", "Hands on Projects", "SE_Hands_on_Projects",
 
     // Artificial Intelligence
-
 
     // Design & Analysis of Algorithm
 
     // Computational Intelligence
 
     // Big Data
+
+    // Compiler Design
+    "Compiler Design", "CD Complete notes", "CD_Complete_notes",
+    "Compiler Design", "CD Full Slides", "CD_Full_Slides",
+    "Compiler Design", "CD Notes handwritten", "CD_Notes_handwritten",
+    "Compiler Design", "Compiler Design", "CD_Compiler_Design",
+
+    // Distributive Operating System
+    "DOS", "Lesson Plan", "DOS_Lesson_Plan",
+    "DOS", "U1 L1 Introduction to distributed systems", "DOS_U1_L1_Introduction_to_distributed_systems",
+    "DOS", "U1 L2 Hardware Concepts", "DOS_U1_L2_Hardware_Concepts",
+    "DOS", "U1 L3 Software Concepts", "DOS_U1_L3_Software_Concepts",
+    "DOS", "U1 L4 Design Issues", "DOS_U1_L4_Design_Issues",
+    "DOS", "UNIT 1 Practise Questions", "DOS_UNIT_1_Practise_Questions",
+    "DOS", "U2 L07 client server", "DOS_U2_L07_client_server",
+    "DOS", "U2 L08 Blocking Vs Nonblocking primitives and Buffered Vs Unbuffered primitives", "DOS_U2_L08_Blocking_Vs_Nonblocking_primitives_and_Buffered_Vs_Unbuffered_primitives",
+    "DOS", "U2 L09 Reliable Vs Unreliable primitives", "DOS_U2_L09_Reliable_Vs_Unreliable_primitives",
+    "DOS", "U2 L10 RPC", "DOS_U2_L10_RPC",
+    "DOS", "U2 L12 RPC Performance", "DOS_U2_L12_RPC_Performance",
+    "DOS", "U2 L5 Layered Protocols", "DOS_U2_L5_Layered_Protocols",
+    "DOS", "U2 L6 Asynchronous Transfer Mode (ATM) Networks", "DOS_U2_L6_Asynchronous_Transfer_Mode_ATM_Networks",
+    "DOS", "Numericals Unit II", "DOS_Numericals_Unit_II",
+    "DOS", "U3 L13 Clock Synchronization", "DOS_U3_L13_Clock_Synchronization",
 
 );
 
